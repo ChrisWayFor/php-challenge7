bd6a5f90ded5f6c8ceb0dd8f22ec885aed66e5b4
ChrisWayFor <christopher.farcoz@gmail.com>
2024-04-02 15:54:22 +0200

    resolv

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Christopher FARCOZ" => [
+        "city" => "Veldin",
+        "weapon" => "T.E.L.T"
+    ]
+];
+
+var_dump($characters);
+
+
+?>
